c269993b8cfc5e1e70e29ed5ec4e033ab1643ee8
Malthe Larsen <75012728+MLFlexer@users.noreply.github.com>
2024-03-22 11:45:05 +0100

    Add files via upload

---
+++ gpu/not_reproducable_rand.cu
@@ -0,0 +1,173 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <omp.h>
+#include <chrono>
+#include <fstream>
+#include <string>
+#include <filesystem>
+#include <cmath>
+
+#include <curand.h>
+#include <curand_kernel.h>
+
+const int SEED = 42;
+const int THREAD_NO = 32;
+
+const int TIME_STEPS = 1000;
+const int save_step = TIME_STEPS/10;
+
+const size_t GRID_SIZE = 1024;//128*2*2*2*2*2*2*2*2*2*2; // Adjust system size
+const size_t NCOLS = GRID_SIZE;
+const size_t NCOLSBYTES = (NCOLS + 8 - 1) / 8;
+const size_t NROWS = GRID_SIZE;
+
+#define IDX(i, j) (i * (NCOLS / 2) + j)
+
+#define GET_VALUE(i, j, arr) ((((arr[(i * (NCOLSBYTES / 2) + (j / 8))] >> (7- (j % 8))) & 1) << 1) - 1)
+
+#define FLIP_BIT(i, j, arr) (arr[(i * (NCOLSBYTES / 2) + (j / 8))] ^= (1 << (7 - (j % 8))))
+
+
+const float BETA = 1/2.25; // Adjust this parameter for funny things! Critical Value is BETA = 0.2;
+
+int calculateChecksum(char* data) {
+    int checksum = 0;
+    for (int i = 0; i < NROWS; i++)
+        for (int j = 0; j < NCOLS; j++) {
+            checksum += data[IDX(i,j)];
+        }
+    return checksum;
+}
+
+void save_data_to_txt(char* red, char* black, const std::string& filename) {
+    std::ofstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << filename << std::endl;
+        return;
+    }
+
+    for (int i = 0; i < NROWS; ++i) {
+        for (int j = 0; j < NCOLS / 2; ++j) {
+            file << (int)GET_VALUE(i, j, red) << " ";
+            file << (int)GET_VALUE(i, j, black) << " ";
+        }
+        file << "\n";
+    }
+
+    file.close();
+}
+
+char* random_spin_field(int N, int M) {
+    char* field = new char[(NROWS * NCOLSBYTES) + 1];
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            if (rand() % 2 == 0) {
+                FLIP_BIT(i, j, field);
+            }
+        }
+    }
+
+    return field;
+}
+
+__global__ void ising_step(char* red, char* black, float beta, bool is_black) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    curandState state;
+    curand_init(clock64() + idx, 0, 0, &state);
+    
+    idx = idx * 8;
+    for (int i = 0; i < 8; i++) {
+        int N = idx;
+        
+        int n = N / (NCOLS / 2);
+        int m = N % (NCOLS / 2);
+
+        int col_offset = (n%2) ^ is_black;
+        col_offset = (col_offset << 1) - 1;
+
+        // Check if the cell should be updated in this kernel call
+        int total = GET_VALUE((n + 1) % NROWS, m, red) +
+                    GET_VALUE((n - 1 + NROWS) % NROWS, m, red) +
+                    GET_VALUE(n, (m + col_offset + NCOLS/2) % (NCOLS/2), red) +
+                    GET_VALUE(n, m, red);
+
+        float dE = 2 * GET_VALUE(n, m, black) * total;
+        
+        float rand_val = curand_uniform(&state);
+
+        if (dE <= 0 || (expf(-dE * beta) > rand_val)) {
+            FLIP_BIT(n, m, black);
+        }
+        idx++;
+    }
+}
+
+
+
+
+
+int main() {
+    std::string directory_name = "GRID_" + std::to_string(GRID_SIZE);
+    std::filesystem::create_directory(directory_name);
+    srand(SEED);
+    char* red_field = random_spin_field(NROWS, NCOLS/2);
+    char* black_field = random_spin_field(NROWS, NCOLS/2);
+    char* red_deviceField;
+    char* black_deviceField;
+    cudaMalloc(&red_deviceField, NROWS * NCOLSBYTES / 2 * sizeof(char));
+    cudaMalloc(&black_deviceField, NROWS * NCOLSBYTES / 2 * sizeof(char));
+    cudaMemcpy(red_deviceField, red_field, ((NROWS * NCOLSBYTES/2)) * sizeof(char), cudaMemcpyHostToDevice);
+    cudaMemcpy(black_deviceField, black_field, ((NROWS * NCOLSBYTES/2)) * sizeof(char), cudaMemcpyHostToDevice);
+    cudaDeviceSynchronize();
+    bool is_black = false;
+
+
+
+    auto start_time1 = std::chrono::high_resolution_clock::now();
+    for (int k = 0; k < TIME_STEPS; k++) {
+        
+        if (is_black) {
+            ising_step<<<(NROWS * NCOLSBYTES + THREAD_NO - 1) / (2*THREAD_NO), THREAD_NO>>>(black_deviceField, red_deviceField, BETA, is_black);
+          is_black = false;
+        }else {
+            ising_step<<<(NROWS * NCOLSBYTES + THREAD_NO - 1) / (2*THREAD_NO), THREAD_NO>>>(red_deviceField, black_deviceField, BETA, is_black);
+          is_black = true;
+        }
+        /*
+        std::cout << k << "/" << TIME_STEPS << std::endl;
+
+        if (k % save_step == 0) {
+            cudaDeviceSynchronize();
+            cudaMemcpy(red_field, red_deviceField, NROWS * NCOLSBYTES / 2 * sizeof(char), cudaMemcpyDeviceToHost);
+            cudaMemcpy(black_field, black_deviceField, NROWS * NCOLSBYTES / 2 * sizeof(char), cudaMemcpyDeviceToHost);
+            
+            std::string filename = directory_name + "/state_" + std::to_string(k) + ".txt";
+            save_data_to_txt(red_field, black_field, filename);
+        }*/
+
+    
+    }
+    cudaDeviceSynchronize();
+    auto end_time1 = std::chrono::high_resolution_clock::now();
+    auto duration1 = std::chrono::duration_cast<std::chrono::milliseconds>(end_time1 - start_time1).count();
+    auto duration2 = std::chrono::duration_cast<std::chrono::seconds>(end_time1 - start_time1).count();
+    std::cout << "Start time: " << start_time1.time_since_epoch().count() << " milliseconds since epoch" << std::endl;
+std::cout << "End time: " << end_time1.time_since_epoch().count() << " milliseconds since epoch" << std::endl;
+
+    std::cout << "Update time: " << duration1 << " milliseconds" << std::endl;
+
+    std::cout << "Update time: " << duration2 << " milliseconds" << std::endl;
+    cudaMemcpy(red_field, red_deviceField, NROWS * NCOLSBYTES / 2 * sizeof(char), cudaMemcpyDeviceToHost);
+    cudaMemcpy(black_field, black_deviceField, NROWS * NCOLSBYTES / 2 * sizeof(char), cudaMemcpyDeviceToHost);
+
+    //std::string filename = directory_name + "/state_" + std::to_string(TIME_STEPS) + ".txt";
+    //save_data_to_txt(red_field, black_field, filename);
+
+    cudaFree(red_field);
+    cudaFree(black_field);
+    cudaDeviceSynchronize();
+    std::cout<< "GPU Finished" << std::endl;
+    return 0;
+}
